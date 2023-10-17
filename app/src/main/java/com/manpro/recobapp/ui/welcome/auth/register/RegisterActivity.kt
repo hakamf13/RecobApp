e13f049894825033b5a53f106cb127224cde5f98
Muhammad Hakam Fardana <mhakamf13@gmail.com>
2023-10-17 10:03:09 +0700

    verify, reset, forgot, home UI

--- app/src/main/java/com/manpro/recobapp/ui/welcome/auth/register/RegisterActivity.kt
+++ app/src/main/java/com/manpro/recobapp/ui/welcome/auth/register/RegisterActivity.kt
@@ -1,12 +1,27 @@
 package com.manpro.recobapp.ui.welcome.auth.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.manpro.recobapp.R
+import com.manpro.recobapp.databinding.ActivityRegisterBinding
+import com.manpro.recobapp.ui.welcome.auth.login.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val binding: ActivityRegisterBinding by lazy {
+        ActivityRegisterBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
+
+        binding.backNav.setOnClickListener{
+            startActivity(Intent(
+                this@RegisterActivity,
+                LoginActivity::class.java
+            ))
+        }
     }
 }
